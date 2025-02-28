621d2b04715eaba3d6815473093a8429ad11d791
timeforchang <chang.andrew.23@gmail.com>
2025-02-28 06:09:19 +0000

    Deploying to gh-pages from @ timeforchang/portfolio@62bf7d33bac332296029cdadc3042f12b397e87a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-resume",
+          title: "resume",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "news-obtained-the-cisco-certified-devnet-associate-certification",
           title: 'Obtained the Cisco Certified DevNet Associate certification',
